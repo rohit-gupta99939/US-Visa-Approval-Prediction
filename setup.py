78453e89f2e5b26f1787e9556c12778492cd5898
rohit <rg99939@gmail.com>
2024-02-22 23:19:52 +0530

    setup the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='us_visa',
+    version='1.0.0',
+    author= "Rohit Gupta",
+    author_email="rg99939@gmail.com",
+    packages=find_packages(),
+)
